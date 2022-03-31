14089183918569135f0bb765fb5b173e0143da6c
besmaerteralb <denis.mimini.1@outlook.com>
2022-03-31 17:07:08 +0200

    adding styling on the section you can post

--- src/components/sharedPosts/SharedPosts.jsx
+++ src/components/sharedPosts/SharedPosts.jsx
@@ -2,33 +2,36 @@ import React from 'react'
 import "../sharedPosts/sharedPosts.css"
 import { EmojiEmotions, Label, PermMedia, Room } from "@material-ui/icons"
 
+
+
 function SharePosts() {
     return (
         <div className='share_container'>
             <div className="share_Wrapper">
                 <div className="share_top">
-                    <img src="" alt="" />
-                    <input type="text" placeholder="What's in your mind?" />
+                    <img className='profilePicture' src="/Assets/MilanSkrinar.jpg" alt="" />
+                    <input type="text" placeholder="What's in your mind?" className='shareInput' />
                 </div>
                 <div className="share_options">
                     <div className="share_option">
-                        <PermMedia />
+                        <PermMedia htmlColor='red' />
                         <span>Photo or Video</span>
                     </div>
                     <div className="share_option">
-                        <Label />
+                        <Label htmlColor='blue' />
                         <span>Tag</span>
                     </div>
                     <div className="share_option">
-                        <Room />
+                        <Room htmlColor='green' />
                         <span>Location</span>
                     </div>
                     <div className="share_option">
                         <EmojiEmotions htmlColor='gold' />
                         <span>Feelings</span>
                     </div>
+                    <button className='shareButton'>Share</button>
                 </div>
-                <button>Share</button>
+
 
             </div>
         </div>
