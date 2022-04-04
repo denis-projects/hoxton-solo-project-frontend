40d91fe551f5b12f7fb1a998a600e01d46cf107b
besmaerteralb <denis.mimini.1@outlook.com>
2022-04-04 18:07:22 +0200

    installing firebase for hosting and auths

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyALNZNhrjazEj8v8DobYMLm6s5FvNnwL4o",
+    authDomain: "stay-connected-e776b.firebaseapp.com",
+    projectId: "stay-connected-e776b",
+    storageBucket: "stay-connected-e776b.appspot.com",
+    messagingSenderId: "433161339522",
+    appId: "1:433161339522:web:bf7cba9b64d01aa20aea9b"
+};
