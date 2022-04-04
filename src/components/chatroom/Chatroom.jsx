9b64aec4c24bb9eb8a5db0929d0d630c2865cfff
besmaerteralb <denis.mimini.1@outlook.com>
2022-04-04 20:21:42 +0200

    adding styling on chat

--- src/components/chatroom/Chatroom.jsx
+++ src/components/chatroom/Chatroom.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import "./chatroom.css"
+import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons'
+import { Avatar, IconButton } from '@material-ui/core'
 
 function Chatroom() {
     return (
-        <div>Chatroom</div>
+        <div className='chat'>
+            <div className='chatHeader'>
+                <Avatar />
+                <div className="chatHeader_info">
+                    <h3>Room name</h3>
+                    <p>last seen...</p>
+                </div>
+                <div className='chatHeader_right'>
+                    <IconButton>
+                        <SearchOutlined />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
+                </div>
+
+            </div>
+
+        </div>
     )
 }
 
