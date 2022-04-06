2ca89d2e1f8297499a61de2a19f30d10fd5c0492
besmaerteralb <denis.mimini.1@outlook.com>
2022-04-06 11:21:58 +0200

    adding profile page

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import Header from '../../components/header/Header'
+import LeftOptions from "../../components/leftOptions/LeftOptions"
+import Posts from '../../components/posts/Posts'
 import "./profile.css"
 
 function Profile() {
     return (
-        <div>Profile</div>
+        <div className='profileContainer'>
+            < Header />
+            <div className='profilePage'>
+                <LeftOptions />
+                <div className="profileRight">
+                    <div className="profileRightTop">
+                        <div className="profileCover">
+                            <img className="profileCoverImg" src="Assets/Logo6.jpg" alt="" />
+                            <img className="profileUserImg" src="Assets/MilanSkrinar.jpg" alt="" />
+                        </div>
+                        <div className="profileInfo">
+                            <h4 className="profileInfoName">Milan Skrinar</h4>
+                            <span className="profileInfoDesc">Wellcome to my page!</span>
+                        </div>
+                    </div>
+                    <div className="profileRightBottom">
+                        <Posts />
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
 export default Profile
+
