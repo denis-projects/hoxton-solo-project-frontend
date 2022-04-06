7b7a3a9242dee81ca8ebe490bac9128d781b52dc
besmaerteralb <denis.mimini.1@outlook.com>
2022-04-06 10:15:34 +0200

    .

--- src/components/postings/Postings.jsx
+++ src/components/postings/Postings.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../postings/postings.css"
 import { Users } from "../../Template.jsx"
 import { MoreVert } from "@material-ui/icons"
@@ -6,7 +6,16 @@ import { MoreVert } from "@material-ui/icons"
 
 function Posting({ posting }) {
 
+    //connecting the state with db
+    const [like, setLike] = useState(posting.like)
 
+    // initial, when we first open app, default is false
+    const [isLiked, setIsLiked] = useState(false)
+
+    const likeHandler = () => {
+        setLike(isLiked ? like - 1 : like + 1)
+        setIsLiked(!isLiked)
+    }
 
     return (
         <div className='posting'>
@@ -27,9 +36,9 @@ function Posting({ posting }) {
                 </div>
                 <div className='posting_options'>
                     <div>
-                        {/* <FavoriteIcon />
-                        <ThumbUpIcon /> */}
-                        <span>{posting.like} persons like this</span>
+
+                        <img className='likeButton' src="Assets/heart.png" onClick={likeHandler} alt="" />
+                        <span className='likesCounter'>{posting.like} persons like this</span>
                     </div>
                     <div>
                         <span>{posting.comment} persons comment the post</span>
